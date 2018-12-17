c8d20d39792c6596f20431469c8ce536252a9671
github username <github email address>
2018-12-16 19:33:16 -0500

    initialized repo

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+  graphiql: true
+}));
+
+app.listen(4000, () => {
+  console.log('Listening');
+});
+
+// Express is an HTTP server
+// It receives and processes requests, sending a response
+
+// With GraphQL, express will pass data to GraphQL, which then directs itself
+// back to express, who sends it to the client
