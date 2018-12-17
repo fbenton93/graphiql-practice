6b099a0b96362c3c62610b6f6dd248bfe1aa154a
github username <github email address>
2018-12-16 19:38:00 -0500

    added User type and properties

--- schema/schema.js
+++ schema/schema.js
@@ -1,4 +1,15 @@
 const graphql = require('graphql');
 const {
-  GraphQLObjectType
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt
 } = graphql;
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+})
