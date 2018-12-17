d26377c74fa736323ff18e182bda40b1a3169d4c
github username <github email address>
2018-12-16 21:06:09 -0500

    added companies type

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -7,19 +7,31 @@ const {
   GraphQLSchema
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+})
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue,args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+        .then(resp => resp.data)
+      }
+    }
   }
 });
 
-const users = [
-  { id: '23', firstName: 'Bill', age: 20 },
-  { id: '47', firstName: 'Samantha', age: 21 }
-];
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -28,7 +40,8 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue,args) {
-        return _.find(users, { id: args.id })
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+        .then(resp => resp.data);
       }
     }
   }
@@ -37,7 +50,8 @@ const RootQuery = new GraphQLObjectType({
 // the above specifies that you can search for a User, the id is the required 'args'
 // to find the user
 
-// the resolve function goes into our data store to find the actual queried data
+
+
 
 module.exports = new GraphQLSchema({
   query: RootQuery
